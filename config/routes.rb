e72c59d4475ec9860196d116276273181138e56d
Antoine Braconnier <antoine.braconnier@hotmail.com>
2019-12-12 22:48:31 +0100

    basic parameters

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home', to: 'pages#home'
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 end
