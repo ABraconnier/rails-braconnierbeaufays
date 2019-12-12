27ca789f255cf15222c8575fcaa20486b744d1f0
Antoine Braconnier <antoine.braconnier@hotmail.com>
2019-12-12 23:24:25 +0100

    some other basic config

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
+
+  before_action :get_clients
+
   def home
+  end
+
+  def admin
+    authenticate_user!
+  end
+
+  private
+
+  def get_clients
+    @clients = Client.all
   end
 end
