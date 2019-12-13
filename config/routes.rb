1b8bb00e1a3bb3afd312d4c59f7e88d8f0f33d76
Antoine Braconnier <antoine.braconnier@hotmail.com>
2019-12-13 11:46:02 +0100

    other small changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :clients, only: [:create, :update, :destroy]
-  get 'home', to: 'pages#home'
-  get 'admin', to: 'pages#admin'
   devise_scope :user do
     get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
+    post 'sign_in', to: 'devise/sessions#create', as: :user_session
     delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
+  resources :clients, only: [:create, :update, :destroy]
+  root to: 'pages#home'
+  get 'admin', to: 'pages#admin'
 end
