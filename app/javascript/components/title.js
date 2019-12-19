9354207e565663ef63cf7bf6883663d7307bade9
Antoine Braconnier <antoine.braconnier@hotmail.com>
2019-12-19 12:48:20 +0100

    title done

---
+++ app/javascript/components/title.js
@@ -0,0 +1,19 @@
+const titleFixed = () => {
+  window.onscroll = () => {
+    const title = document.querySelector('#title');
+    if(window.scrollY <= window.innerHeight / 2) {
+      title.style.top = '30vh';
+      document.getElementById('bastien').style.transitionDuration = '4s';
+      document.getElementById('chloe').style.transitionDuration = '4s';
+      document.getElementById('bastien').style.opacity = '1';
+      document.getElementById('chloe').style.opacity = '1';
+    } else {
+      title.style.top = '-14vw';
+      document.getElementById('bastien').style.transitionDuration = '1s';
+      document.getElementById('bastien').style.opacity = '0';
+      document.getElementById('chloe').style.transitionDuration = '1s';
+      document.getElementById('chloe').style.opacity = '0';
+    }
+  };
+};
+export { titleFixed };
