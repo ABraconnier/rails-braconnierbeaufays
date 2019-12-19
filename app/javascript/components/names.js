70a20382a230b2e66f12bf943cf76907c786e6b7
Antoine Braconnier <antoine.braconnier@hotmail.com>
2019-12-19 19:09:21 +0100

    bbs done and added function for names to appear

---
+++ app/javascript/components/names.js
@@ -0,0 +1,8 @@
+const namesAppear = () => {
+  window.onload = () => {
+    document.getElementById('bastien').style.opacity = '1';
+    document.getElementById('chloe').style.opacity = '1';
+  };
+};
+
+export {namesAppear};
