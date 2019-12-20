49b2c4037c40f886cf0755ca8ab1173f9b01bca3
Antoine Braconnier <antoine.braconnier@hotmail.com>
2019-12-20 13:19:55 +0100

    removed turbolinks! it works finally

--- app/javascript/components/title.js
+++ app/javascript/components/title.js
@@ -1,6 +1,7 @@
 const titleFixed = () => {
-  window.onscroll = () => {
-    const title = document.querySelector('#title');
+  const title = document.querySelector('#title');
+  const logos = document.querySelectorAll('.logo-clients');
+  const whereIsTheTitle = () => {
     if(window.scrollY <= window.innerHeight / 2) {
       title.style.top = '30vh';
       document.getElementById('bastien').style.transitionDuration = '4s';
@@ -14,6 +15,10 @@ const titleFixed = () => {
       document.getElementById('chloe').style.transitionDuration = '1s';
       document.getElementById('chloe').style.opacity = '0';
     }
+  console.log(title.style.top);
   };
+  window.onclick = () => {whereIsTheTitle()};
+  window.addEventListener('scroll', () => {whereIsTheTitle()});
+  logos.forEach(logo => logo.addEventListener('click', () => {whereIsTheTitle()}));
 };
 export { titleFixed };
